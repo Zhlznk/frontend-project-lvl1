4766b3a56ee26c8abee6d1373d4b5f24383318dc
Zheleznyak Andrey <zhlznk@gmail.com>
2020-08-26 11:17:11 +0300

    Step6 добавил randomNum

--- src/content.js
+++ src/content.js
@@ -1,16 +1,16 @@
+const randomNum = () => (Math.floor(Math.random() * 100));
+
 const content = (str) => {
+  const result = [];
   if (str === 'even') {
-    const result = [];
-    const num = Math.floor(Math.random() * 100);
+    const num = randomNum();
     const answerCorrect = () => ((num % 2 === 0) ? 'yes' : 'no');
     result.push(num, answerCorrect(num));
-    return result;
   }
   if (str === 'calc') {
-    const result = [];
     const tmp = ['+', '-', '*'];
-    const first = Math.floor(Math.random() * 100);
-    const second = Math.floor(Math.random() * 100);
+    const first = randomNum();
+    const second = randomNum();
     const getRandomInt = (max) => Math.floor(Math.random() * Math.floor(max));
     const item = getRandomInt(3);
     let answerCorrect = 0;
@@ -18,9 +18,8 @@ const content = (str) => {
     if (item === 1) answerCorrect = (first - second);
     if (item === 2) answerCorrect = (first * second);
     result.push(`${first} ${tmp[item]} ${second}`, `${answerCorrect}`);
-    return result;
   }
-  return 100500;
+  return result;
 };
 
 export default content;
