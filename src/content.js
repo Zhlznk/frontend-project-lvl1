79d85348a31ab8d253535c6d96997776cfce8950
Zheleznyak Andrey <zhlznk@gmail.com>
2020-08-26 04:15:24 +0300

    Step6

--- src/content.js
+++ src/content.js
@@ -8,12 +8,19 @@ const content = (str) => {
   }
   if (str === 'calc') {
     const result = [];
-    const num = 100;
-    const answerCorrect = 500;
-    result.push(num, answerCorrect);
+    const tmp = ['+', '-', '*'];
+    const first = Math.floor(Math.random() * 100);
+    const second = Math.floor(Math.random() * 100);
+    const getRandomInt = (max) => Math.floor(Math.random() * Math.floor(max));
+    const item = getRandomInt(3);
+    let answerCorrect = 0;
+    if (item === 0) answerCorrect = (first + second);
+    if (item === 1) answerCorrect = (first - second);
+    if (item === 2) answerCorrect = (first * second);
+    result.push(`${first} ${tmp[item]} ${second}`, `${answerCorrect}`);
     return result;
   }
-  return 500;
+  return 100500;
 };
 
 export default content;
